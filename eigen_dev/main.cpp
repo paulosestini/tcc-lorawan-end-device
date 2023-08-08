adfeb3f1d6291be8c500a3cb3a3dbe67177a7683
paulosestini <24728744+paulosestini@users.noreply.github.com>
2023-08-07 21:12:06 -0300

    update

--- eigen_dev/main.cpp
+++ eigen_dev/main.cpp
@@ -6,19 +6,26 @@ using Eigen::VectorXd;
 using Eigen::ArrayXd;
 
 const int MAX_FRAMES = 500;
-const int N_SUBPORTERS = 64;
+const int N_SUBPORTERS = 52;
+const int WINDOW_SIZE = 50;
 const float THRESHOLD_DETECTION = 0.03;
+double reference_power = 0;
+double current_power = 0;
 MatrixXd frames(MAX_FRAMES, N_SUBPORTERS);
 ArrayXd powers(MAX_FRAMES);
+ArrayXd csi_vector(N_SUBPORTERS);
 int collected_frames = 0;
+bool detected = 0;
+bool should_send_lora_packet = 0;
 
 
 ArrayXd generate_fake_csi_vector() {
   return (ArrayXd::Random(64) + 1) / 2;
 }
 
-void process_Csi(ArrayXd csi_vector, double rssi) {
-  
+void process_Csi(ArrayXd full_csi_vector, double rssi) {
+  csi_vector << full_csi_vector(Eigen::seq(6, 31)), full_csi_vector(Eigen::seq(33, 58));
+
   double rssi_power = pow(10, rssi/10);
   double scale_factor = rssi_power / (csi_vector.sum() / N_SUBPORTERS);
   scale_factor = sqrt(scale_factor);
@@ -27,12 +34,32 @@ void process_Csi(ArrayXd csi_vector, double rssi) {
   csi_vector = 10 * (csi_vector + 1e-10 ).log10();
 
   if(collected_frames < MAX_FRAMES) {
-    // To be continued...
-  }
+    frames.row(collected_frames) = csi_vector;
+  } else {
+    if(collected_frames == MAX_FRAMES) {
+      reference_power = frames.sum() / MAX_FRAMES;
 
+      printf("\n\n ----REFERENCE ESTABLISHED---- \n\n");
+    } else {
+      current_power = frames(Eigen::seq(MAX_FRAMES - WINDOW_SIZE, MAX_FRAMES-1), Eigen::all).sum();
+      current_power /= WINDOW_SIZE;
 
-  std::cout << csi_vector << std::endl;
+      if (current_power <= (1 + THRESHOLD_DETECTION) * reference_power) {
+        if(!detected) {
+          printf("\n DETECTED \n");
+          should_send_lora_packet = true;
+          detected = true;
+        } else {
+          detected = false;
+        }
+        
+        printf("Current power: %f, Reference power: %f \n", current_power, reference_power);
+      }
+    }
 
+    frames << frames(Eigen::seq(1, MAX_FRAMES-1), Eigen::all), csi_vector.transpose();
+  } 
+  
   collected_frames += 1;
 
   return;
