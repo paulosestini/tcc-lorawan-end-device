3f40db02aba781da5a7beedbdc5fc1ece35409c2
Paulo Sestini <24728744+paulosestini@users.noreply.github.com>
2023-12-09 14:10:07 -0300

    model/lambda updates

--- data_collection/display_matrix.py
+++ data_collection/display_matrix.py
@@ -6,17 +6,19 @@ import matplotlib.pyplot as plt
 def plot_matrix(csv_file):
     # Load matrix from CSV file
     matrix = pd.read_csv(csv_file, header=None).values
+
     
     # Create a heatmap of the matrix
-    sns.heatmap(matrix.T, cmap="viridis", cbar_kws={'label': 'Value'})
-    plt.title('Matrix Heatmap')
-    plt.xlabel('Column')
-    plt.ylabel('Row')
+    fig, ax = plt.subplots(dpi=150)
+    sns.heatmap(matrix.T, cmap="plasma", cbar_kws={'label': 'dBm'}, vmin=-50, ax=ax)
+    plt.title('Matriz de amplitudes CSI - Exemplo de carro')
+    plt.ylabel('Índice da subportadora')
+    plt.xlabel('Vetor CSI')
     
     # Show the plot
     plt.show()
 
 if __name__ == "__main__":
     # Provide the path to your CSV file here
-    csv_file = 'matrices/matrix_2023-11-01_15-39-54.csv'
+    csv_file = 'filtered_data/matrix_2023-11-09_18-24-00_car.csv'
     plot_matrix(csv_file)
